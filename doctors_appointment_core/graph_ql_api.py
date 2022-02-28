25d24a03b2ce94f2eb30d5fb981a95121c75a93e
saiforceone <saiforceone@gmail.com>
2022-02-28 00:23:07 -0500

    add str method to django models, start to define queries and resolvers for graphql. todo: finish queries and resolvers

--- doctors_appointment_core/graph_ql_api.py
+++ doctors_appointment_core/graph_ql_api.py
@@ -1,18 +1,86 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .models import CancellationReason
-
+from .models import *
 class CancellationReasonType(DjangoObjectType):
   class Meta:
     model = CancellationReason
     fields = ("id", "title", "description")
 
 
+class EmergencyContactType(DjangoObjectType):
+
+  class Meta:
+    model = EmergencyContact
+    fields = "__all__"
+
+class HealthFacilityType(DjangoObjectType):
+
+  class Meta:
+    model = HealthFacility
+    fields = "__all__"
+
+
+class SpecialistType(DjangoObjectType):
+
+  class Meta:
+    model = Specialist
+    fields = "__all__"
+
+
+class SpecializationType(DjangoObjectType):
+
+  class Meta:
+    model = Specialization
+    fields = "__all__"
+
 class Query(graphene.ObjectType):
   all_cancellation_reasons = graphene.List(CancellationReasonType)
 
   def resolve_all_cancellation_reasons(root, info):
     return CancellationReason.objects.all()
+
+  # -- emergency contacts
+  emergency_contacts_for_user = graphene.List(EmergencyContactType)
+
+  def resolve_emergency_contacts_for_user(root, info):
+    if not info.context.user.is_authenticated:
+      return None
+    else:
+      return EmergencyContact.objects.filter(related_user=info.context.user.id)
+
+  # -- specialists
+  all_specialists = graphene.List(SpecialistType)
+
+  def resolve_all_specialists(root, info):
+    return Specialist.objects.all()
+
+  specialists_by_specialization = graphene.List(SpecializationType, specialization=graphene.String(required=True))
+
+  def resolve_specialists_by_specialization(root, info, specialization):
+    try:
+      return Specialization.objects.filter(name=specialization)
+    except Exception:
+      return None
+
+  # -- specializations 
+  all_specializations = graphene.List(SpecializationType)
+
+  def resolve_all_specializations(root, info):
+    return Specialization.objects.all()
+
+  # -- health facilities
+  all_health_facilities = graphene.List(HealthFacilityType)
+
+  def resolve_all_facilities(root, info):
+    return HealthFacility.objects.all()
+
+  facilities_by_region = graphene.List(HealthFacilityType, region=graphene.String(required=True))
+
+  def resolve_facilities_by_region(root, info, region):
+    try:
+      return HealthFacility.objects.filter(region=region)
+    except Exception:
+      return None
 
 schema = graphene.Schema(query=Query)
--- doctors_appointment_core/models.py
+++ doctors_appointment_core/models.py
@@ -11,6 +11,12 @@ class UserProfile(models.Model):
   contact_number = models.CharField(max_length=16)
   profile_type = models.CharField(choices=USER_PROFILE_TYPES, max_length=13)
 
+  def __str__(self) -> str:
+    return "({id}) Profile: {user}".format(
+      id=self.id,
+      user=self.user.username
+    )
+
 
 class EmergencyContact(models.Model):
   """Defines an emergency contact for a related patient / user"""
@@ -20,6 +26,11 @@ class EmergencyContact(models.Model):
   email_address = models.EmailField()
   relationship_to_user = models.CharField(max_length=20)
 
+  def __str__(self) -> str:
+    return "({id}) Emergency Contact: {full_name}".format(
+      id=self.id,
+      full_name=self.full_name
+    )
 
 class HealthFacility(models.Model):
   """Defines a health facility where appointments are booked"""
@@ -33,6 +44,9 @@ class HealthFacility(models.Model):
   class Meta:
     verbose_name_plural = 'Health Facilities'
 
+  def __str__(self) -> str:
+    return "({id}) Facility {name}".format(id=self.id, name=self.name)
+
 
 class HealthFacilityHour(models.Model):
   """Defines open hours for a given facility"""
@@ -42,6 +56,11 @@ class HealthFacilityHour(models.Model):
   day_to = models.CharField(max_length=8)
   time_to = models.TimeField()
 
+  def __str__(self) -> str:
+    return "{facility} - {day}".format(
+      facility=self.related_facility.name,
+      day=self.day_from
+    )
 
 class Specialist(models.Model):
   """Defines a specialist or doctor"""
@@ -49,12 +68,20 @@ class Specialist(models.Model):
   related_facility = models.ForeignKey(HealthFacility, on_delete=models.CASCADE)
   date_started = models.DateField()
 
+  def __str__(self) -> str:
+    return "Specialist: {}".format(self.full_name)
 
 class Specialization(models.Model):
   """Defines specializations for a given specialist / doctor"""
   related_specialist = models.ForeignKey(Specialist, on_delete=models.CASCADE)
   name = models.CharField(max_length=20)
   description = models.TextField()
+
+  def __str__(self) -> str:
+    return "Specialization: {spec_name} for Specialist: {spec}".format(
+      spec=self.related_specialist.full_name,
+      spec_name=self.name
+    )
 
 
 class Appointment(models.Model):
@@ -67,11 +94,22 @@ class Appointment(models.Model):
   duration_minutes = models.PositiveSmallIntegerField()
   cancelled_at = models.DateTimeField(blank=True, null=True)
 
+  def __str__(self) -> str:
+    return "Appointment: {id} for User: {user}".format(
+      id=self.id,
+      user=self.patient.user.username
+    )
+
 
 class AppointmentNote(models.Model):
   """Defines appointment notes for a given appointment"""
   related_appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
   note = models.TextField()
+
+  def __str__(self) -> str:
+    return "Note for Appointment: {appt_id}".format(
+      appt_id=self.related_appointment.id,
+    )
 
 
 class CancellationReason(models.Model):
@@ -87,4 +125,10 @@ class CancellationReason(models.Model):
   """
   title = models.CharField(max_length=75)
   description = models.TextField()
+
+  def __str__(self) -> str:
+    return "({id}) Cancellation Reason: {title}".format(
+      id=self.id,
+      title=self.title
+    )
 
